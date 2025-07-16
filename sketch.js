6085d2283ccc82d074376f4d375b4e12de7acbae
timothy battafarano <tim@deepsprings.edu>
2025-07-16 12:34:58 -0500

    set up balloon as an image

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
+let balloon;
+let balloonX = 0;
+let balloonY = 200;
+
 function setup() {
   createCanvas(400, 400);
+  balloon = loadImage("balloon.png");
 }
 
 function draw() {
-  background(220);
-  text('Test', width/2, height/2);
+  background(0);
+  drawBalloon();
 }
 
+function drawBalloon() {
+  image(balloon, balloonX, balloonY, 80, 80);
+}
 
